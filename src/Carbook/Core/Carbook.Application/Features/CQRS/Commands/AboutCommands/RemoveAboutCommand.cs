3631ab60558e59ddada103627e8c1aa797dcc49f
Mustafa Beyazbulut <77739828+mustafabeyazbulut@users.noreply.github.com>
2023-11-24 02:29:39 +0300

    About nesnesi için CQRS işlemleri yapıldı
    About nesnesi için CQRS işlemleri yapıldı

---
+++ src/Carbook/Core/Carbook.Application/Features/CQRS/Commands/AboutCommands/RemoveAboutCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.CQRS.Commands.AboutCommands
+{
+    public class RemoveAboutCommand
+    {
+        public int id { get; set; }
+        public RemoveAboutCommand(int id)
+        {
+            this.id = id;
+        }
+    }
+}
---
+++ src/Carbook/Core/Carbook.Application/Features/CQRS/Handlers/AboutHandlers/RemoveAboutCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Carbook.Application.Features.CQRS.Commands.AboutCommands;
+using Carbook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class RemoveAboutCommandHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public RemoveAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(RemoveAboutCommand command)
+        {
+            var value = await _repository.GetByIdAsync(command.id);
+            await _repository.RemoveAsync(value);
+        }
+    }
+}
