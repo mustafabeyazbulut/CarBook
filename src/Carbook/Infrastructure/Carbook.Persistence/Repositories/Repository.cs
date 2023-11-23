2df96763f8404fa23736c57e93f3bcae49716ed7
Mustafa Beyazbulut <77739828+mustafabeyazbulut@users.noreply.github.com>
2023-11-24 01:52:25 +0300

    Persistence katmanında Repository sınıfı oluşturuldu.
    Persistence katmanında Repository sınıfı oluşturuldu.

---
+++ src/Carbook/Infrastructure/Carbook.Persistence/Repositories/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Carbook.Application.Interfaces;
+using Carbook.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.Persistence.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly CarBookContext _context;
+
+        public Repository(CarBookContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            _context.Set<T>().Add(entity);
+          await  _context.SaveChangesAsync();
+
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task RemoveAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
