5603a3d5d5013cf1ca7df460e706ae790ed28030
Mustafa Beyazbulut <77739828+mustafabeyazbulut@users.noreply.github.com>
2023-11-24 01:44:05 +0300

    Context oluşturuldu. Migration işlemi yapıldı. IRepository oluşturuldu
    Context oluşturuldu. Migration işlemi yapıldı. IRepository oluşturuldu

--- src/Carbook/Core/CarBook.Domain/Entities/Contact.cs
+++ src/Carbook/Core/CarBook.Domain/Entities/Contact.cs
@@ -12,7 +12,6 @@ namespace CarBook.Domain.Entities
         public string Name { get; set; }
         public string Email { get; set; }
         public string Subject { get; set; }
-        public string Subject { get; set; }
         public string Message { get; set; }
         public DateTime SendDate { get; set; }
     }
