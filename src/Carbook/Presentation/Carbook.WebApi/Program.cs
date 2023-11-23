9152dedfb9fcfcab22f7407ab345d75997788751
Mustafa Beyazbulut <77739828+mustafabeyazbulut@users.noreply.github.com>
2023-11-24 02:44:41 +0300

    Program.cs içerisine About için scope lar eklendi
    Program.cs içerisine About için scope lar eklendi

--- src/Carbook/Presentation/Carbook.WebApi/Program.cs
+++ src/Carbook/Presentation/Carbook.WebApi/Program.cs
@@ -1,6 +1,17 @@
+using Carbook.Application.Features.CQRS.Handlers.AboutHandlers;
+using Carbook.Persistence.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<CarBookContext>();
+
+builder.Services.AddScoped<GetAboutByIdQueryHandler>();
+builder.Services.AddScoped<GetAboutQueryHandler>();
+builder.Services.AddScoped<CreateAboutCommandHandler>();
+builder.Services.AddScoped<UpdateAboutCommandHandler>();
+builder.Services.AddScoped<RemoveAboutCommandHandler>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
