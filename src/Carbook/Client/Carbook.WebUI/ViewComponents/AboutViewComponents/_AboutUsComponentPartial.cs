5be2e39e475161ae3d0f1c81cb158fabb02a2356
Mustafa Beyazbulut <77739828+mustafabeyazbulut@users.noreply.github.com>
2023-12-05 16:07:27 +0300

    MVC Layout componentler hazırlandı
    MVC Layout componentler hazırlandı

---
+++ src/Carbook/Client/Carbook.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Carbook.WebUI.ViewComponents.AboutViewComponents
+{
+    public class _AboutUsComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task< IViewComponentResult> InvokeAsync()
+        {
+            var client= _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("");
+            return View();
+        }
+    }
+}
--- src/Carbook/Client/Carbook.WebUI/obj/Debug/net8.0/Carbook.WebUI.AssemblyInfo.cs
+++ src/Carbook/Client/Carbook.WebUI/obj/Debug/net8.0/Carbook.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Carbook.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc9c4ba0056a4b3dc2ab72622a4af9e593638cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e1b0715e4368ecadd5598e97559e863ee46907e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Carbook.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Carbook.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
