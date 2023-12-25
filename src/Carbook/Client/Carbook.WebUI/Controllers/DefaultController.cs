c93cb55be96482dc4ff16771a8252c6ea111e59c
Mustafa Beyazbulut <77739828+mustafabeyazbulut@users.noreply.github.com>
2023-12-25 15:14:48 +0300

    .

--- src/Carbook/Client/Carbook.WebUI/Controllers/DefaultController.cs
+++ src/Carbook/Client/Carbook.WebUI/Controllers/DefaultController.cs
@@ -1,13 +1,34 @@
 ﻿using Carbook.Dto.BannerDtos;
+using Carbook.Dto.LocationDtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace Carbook.WebUI.Controllers
 {
     public class DefaultController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public DefaultController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7166/api/Locations");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+            List<SelectListItem> values2= (from x in values
+                                          select new SelectListItem
+                                          {
+                                              Text=x.Name,
+                                              Value=x.LocationId.ToString(),
+                                          }).ToList();
+            ViewBag.v = values2;
             return View();
         }
     }
